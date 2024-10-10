73bceb7b39407c9fcd4e804ca755fd3083358428
WillTheBiggest <William.webb@mcusd709.org>
2024-10-10 09:37:52 -0500

    Task six fail
    Failed changes for task six

--- ChatBot.java
+++ ChatBot.java
@@ -40,8 +40,15 @@ public class ChatBot{
     }else if(findKeyword(statement,"Overwatch",0 )> -1 ){
     response = "You need to take a little walk to the shower";
     
-    }else if(findKeyword(statement,"I want to",0) > -1){
+    }else if(findKeyword(statement,"I want to",0) > -1) {
     response = transformIWantToStatement(statement);
+
+    }else if(findKeyword(statement,"you", 0) > -1  && findKeyword(statement,"me",0) > -1 ) {
+    response = transformYouMeStatement(statement);
+    
+    }else if(findKeyword(statement,"I want something", 0) > -1){
+    response = transformIWantStatement(statement);
+
     }else{
       response = getRandomResponse();
     }
@@ -170,5 +177,17 @@ public class ChatBot{
 
 		String restOfStatement = statement.substring(psnOfYou + 3, psnOfMe).trim();
 		return "What makes you think that I " + restOfStatement + " you?";
-	}
+	
+
+  private String transformIWantStatement(String statement){
+		//  Remove the final period, if there is one
+		statement = statement.trim();
+		String lastChar = statement.substring(statement.length() - 1);
+		if (lastChar.equals(".")){
+			statement = statement.substring(0, statement.length() - 1);
+		}
+		int psn = findKeyword(statement, "I want", 0);
+		String restOfStatement = statement.substring(psn + 6).trim();
+		return "Would you really be happy if you had " + restOfStatement + "?";}
+  }
 }
